0a620ad2a32112d88a61adc8d198288e78a1d692
JaiS739 <jksingh00739@gmail.com>
2022-09-20 21:09:31 +0530

    added all the files of modal

---
+++ index.js
@@ -0,0 +1,11 @@
+const modal_container = document.getElementById("modal-container");
+const open = document.getElementById("open");
+const close = document.getElementById("close");
+
+open.addEventListener("click", () => {
+  modal_container.classList.add("show");
+});
+
+close.addEventListener("click", () => {
+  modal_container.classList.remove("show");
+});
